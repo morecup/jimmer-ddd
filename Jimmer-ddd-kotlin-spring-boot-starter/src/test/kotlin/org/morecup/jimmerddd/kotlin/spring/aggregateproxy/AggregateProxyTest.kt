9ed8759e42678a1ebc0ee4a7e1365eb171b87951
bald9 <1059393848@qq.com>
2025-06-02 16:00:33 +0800

    bug修复

--- Jimmer-ddd-kotlin-spring-boot-starter/src/test/kotlin/org/morecup/jimmerddd/kotlin/spring/aggregateproxy/AggregateProxyTest.kt
+++ Jimmer-ddd-kotlin-spring-boot-starter/src/test/kotlin/org/morecup/jimmerddd/kotlin/spring/aggregateproxy/AggregateProxyTest.kt
@@ -3,7 +3,6 @@ package org.morecup.jimmerddd.kotlin.spring.aggregateproxy
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.databind.util.ClassUtil.name
 import org.babyfish.jimmer.sql.ast.mutation.AssociatedSaveMode
 import org.babyfish.jimmer.sql.ast.mutation.SaveMode
 import org.babyfish.jimmer.sql.fetcher.impl.FetcherImpl
@@ -12,22 +11,14 @@ import org.babyfish.jimmer.sql.kt.KSqlClient
 import org.babyfish.jimmer.sql.kt.ast.expression.eq
 import org.babyfish.jimmer.sql.kt.fetcher.newFetcher
 import org.junit.jupiter.api.Test
-import org.morecup.jimmerddd.kotlin.spring.App
 import org.morecup.jimmerddd.core.aggregateproxy.allAggregationFields
-import org.morecup.jimmerddd.kotlin.spring.domain.goods.Goods
-import org.morecup.jimmerddd.kotlin.spring.domain.order.Order
-import org.morecup.jimmerddd.kotlin.spring.domain.order.OrderImpl
-import org.morecup.jimmerddd.kotlin.spring.domain.order.OrderRepository
-import org.morecup.jimmerddd.core.domain.order.by
-import org.morecup.jimmerddd.core.domain.order.fetchBy
-import org.morecup.jimmerddd.core.domain.order.id
-import org.morecup.jimmerddd.kotlin.spring.domain.order.orderAggregateProxy
-import org.morecup.jimmerddd.kotlin.spring.domain.order.testOrderId
 import org.morecup.jimmerddd.core.preanalysis.MethodInfo
 import org.morecup.jimmerddd.core.preanalysis.analysisMethodFetcher
+import org.morecup.jimmerddd.kotlin.spring.App
+import org.morecup.jimmerddd.kotlin.spring.domain.goods.Goods
+import org.morecup.jimmerddd.kotlin.spring.domain.order.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import kotlin.jvm.java
 import kotlin.reflect.jvm.javaMethod
 
 @SpringBootTest(classes = [App::class])
