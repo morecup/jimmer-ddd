aea203fda1b4c222e0eabbc4ac9bf38235dd3404
bald9 <1059393848@qq.com>
2025-06-07 11:37:28 +0800

    修复java版本bug

--- jimmer-ddd-java-spring-boot-starter/src/main/java/org/morecup/jimmerddd/java/spring/aggregateproxy/IdGenerator.java
+++ jimmer-ddd-java-spring-boot-starter/src/main/java/org/morecup/jimmerddd/java/spring/aggregateproxy/IdGenerator.java
@@ -1,8 +1,11 @@
 package org.morecup.jimmerddd.java.spring.aggregateproxy;
 
+import org.babyfish.jimmer.spring.cfg.support.SpringConnectionManager;
 import org.babyfish.jimmer.sql.JSqlClient;
+import org.babyfish.jimmer.sql.kt.KSqlClient;
 import org.babyfish.jimmer.sql.runtime.JSqlClientImplementor;
 import org.morecup.jimmerddd.core.aggregateproxy.BaseAssociatedFixedKt;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import java.sql.Connection;
 
@@ -12,6 +15,9 @@ public class IdGenerator {
     }
 
     public static Object generatorId(JSqlClientImplementor jSqlClient, Class<?> entityType, Connection con) {
+        if (con == null) {
+            con = DataSourceUtils.getConnection(((SpringConnectionManager) jSqlClient.getConnectionManager()).getDataSource());
+        }
         return BaseAssociatedFixedKt.commonGeneratorId(jSqlClient, entityType, con);
     }
 }
