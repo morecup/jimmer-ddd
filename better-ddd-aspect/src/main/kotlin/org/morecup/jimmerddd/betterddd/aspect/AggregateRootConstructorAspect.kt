e08d9df4a3366467722595bc094eb1d59fa256e9
wlp <wlp@163@com>
2025-06-28 11:00:49 +0800

    分拆better-ddd模块

--- better-ddd-aspect/src/main/kotlin/org/morecup/jimmerddd/betterddd/aspect/AggregateRootConstructorAspect.kt
+++ better-ddd-aspect/src/main/kotlin/org/morecup/jimmerddd/betterddd/aspect/AggregateRootConstructorAspect.kt
@@ -2,7 +2,6 @@ package org.morecup.jimmerddd.betterddd.aspect
 
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.ProceedingJoinPoint
-import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
